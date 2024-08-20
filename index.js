47157a17e3f36616d2a8abcc07843db5f66899a4
WillYuum <liamshere98@gmail.com>
2024-08-20 22:16:11 +0300

    Implement image switching for survive winter content

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ const observer = new IntersectionObserver((entries) => {
 items.forEach(item => {
     observer.observe(item);
 });
+
+
+
+function handleSwitchingBetweenSeasonImages(){
+    const changeIntervalInSeconds = 1.75;
+    const imagesFileName = [
+        "summer",
+        "autumn",
+        "winter",
+    ];
+
+
+    const itemContent = document.querySelector('.survive-season');
+    
+    
+    let index = 0;
+    const imagesCount = imagesFileName.length - 1;
+    setInterval(()=>{
+        index = index >= imagesCount - 1 ? 0 : ++index;
+        itemContent.querySelector('img').src = `assets/${imagesFileName[index]}.png`
+    },changeIntervalInSeconds * 1000);
+}
+handleSwitchingBetweenSeasonImages();

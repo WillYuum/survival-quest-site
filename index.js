7e76f1f849ec5bfde8a6b6f258debd61d751b2cc
WillYuum <liamshere98@gmail.com>
2024-08-21 05:40:20 +0200

    Fix pagination sizing

--- index.js
+++ index.js
@@ -18,3 +18,26 @@ function handleSwitchingBetweenSeasonImages(){
     },changeIntervalInSeconds * 1000);
 }
 handleSwitchingBetweenSeasonImages();
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
